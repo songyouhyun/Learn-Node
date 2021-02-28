4331578c13b3f97a8eefb03b2e292df172c26a44
songyouyun <doong3373@gmail.com>
2021-02-28 14:45:11 +0900

    Node.js - URL로 입력된 값 사용하기

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');   //'url'이라는 [모듈]을 [변수]'url'이라는 이름으로 사용할 것이다.
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(queryData.id);
 });
 app.listen(3000);
