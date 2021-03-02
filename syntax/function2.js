50aec2b3744f967dae87b68213f4b236678b9005
songyouyun <doong3373@gmail.com>
2021-03-02 11:21:57 +0900

    JavaScript - 함수의 출력

--- syntax/function2.js
+++ syntax/function2.js
@@ -2,7 +2,8 @@ console.log(Math.round(1.6)); //2
 console.log(Math.round(1.4)); //1
 
 function sum(first, second){ // parameter
-  console.log(first+second);
+  return first+second;
+  //return은 어떤 함수를 출력시킨다는 의미와 그 즉시, 함수를 종료한다는 두가지 의미를 가진 특수한 키워드
 }
 
-sum(2,4); // argument
+console.log(sum(2,4)); // argument
